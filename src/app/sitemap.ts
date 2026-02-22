1ca504aedcf80dc8bac69430497367f41d46b21f
ngogiaquyen <ngogiaquyendhtn223@gmail.com>
2026-02-22 08:09:27 +0700

    seo on page

---
+++ src/app/sitemap.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://bloxtools.info";
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/create`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+  ];
+}
